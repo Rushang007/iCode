e434fe43a08d439ba7116b3ad37ea4cea2afbac9
Rushang <rushang.patel@metizsoft.com>
2020-11-30 09:54:42 +0530

    Initial Commit

---
+++ iCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCode
+//
+//  Created by Rushang Patel (iOS Developer) on 30/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
